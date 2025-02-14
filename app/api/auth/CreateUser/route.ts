5fff05c19e702352b86a8a9e038afb359c535bb3
niraj <nirajversilal@gmail.com>
2025-02-14 16:14:04 +0500

    remove axios

--- app/api/auth/CreateUser/route.ts
+++ app/api/auth/CreateUser/route.ts
@@ -1,21 +1,21 @@
-import axios from "axios";
+// import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest) => {
   try {
     const token = await req.text();
     console.log(token);
-    const res = await axios.post(
-      "https://jotion-backend.osc-fr1.scalingo.io/api/auth/CreateUser",
-      {
-        token,
-      },
-      {
-        withCredentials: true,
-      }
-    );
-    console.log(res);
-    return NextResponse.json(res);
+    // const res = await axios.post(
+    //   "https://jotion-backend.osc-fr1.scalingo.io/api/auth/CreateUser",
+    //   {
+    //     token,
+    //   },
+    //   {
+    //     withCredentials: true,
+    //   }
+    // );
+    // console.log(res);
+    return NextResponse.json(token);
   } catch (error) {
     console.log(error);
   }

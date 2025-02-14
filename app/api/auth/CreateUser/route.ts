93374ba08446e88905933ba91873f8adb246705f
niraj <nirajversilal@gmail.com>
2025-02-14 14:10:33 +0500

    change route

--- app/api/auth/CreateUser/route.ts
+++ app/api/auth/CreateUser/route.ts
@@ -1,17 +1,19 @@
+import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest) => {
   try {
     const token = await req.text();
-    // const res = await fetch("http://localhost:8000/api/auth/CreateUser", {
-    //   method: "POST",
-    //   credentials: "include",
-    //   body: JSON.stringify(token),
-    // });
-    // const body = await res.json();
-    // console.log(body);
-    console.log(token);
-    return NextResponse.json("hello");
+    const res = await axios.post(
+      "http://localhost:8000/api/auth/CreateUser",
+      {
+        token,
+      },
+      {
+        withCredentials: true,
+      }
+    );
+    return NextResponse.json(res);
   } catch (error) {
     console.log(error);
   }

8de6e8605de83497f86fdffe1722dd2335d507ee
niraj <nirajversilal@gmail.com>
2025-02-14 16:02:15 +0500

    test

--- app/api/auth/CreateUser/route.ts
+++ app/api/auth/CreateUser/route.ts
@@ -4,6 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 export const POST = async (req: NextRequest) => {
   try {
     const token = await req.text();
+    console.log(token);
     const res = await axios.post(
       "https://jotion-backend.osc-fr1.scalingo.io/api/auth/CreateUser",
       {
@@ -13,7 +14,8 @@ export const POST = async (req: NextRequest) => {
         withCredentials: true,
       }
     );
-    return NextResponse.json(res.statusText, { status: res.status });
+    console.log(res);
+    return NextResponse.json(res);
   } catch (error) {
     console.log(error);
   }

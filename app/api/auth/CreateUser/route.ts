2f42136c15ec0b6e5d45c82f23378492c757df2f
niraj <nirajversilal@gmail.com>
2025-02-14 13:37:19 +0500

    modified route.ts

--- app/api/auth/CreateUser/route.ts
+++ app/api/auth/CreateUser/route.ts
@@ -3,13 +3,14 @@ import { NextRequest, NextResponse } from "next/server";
 export const POST = async (req: NextRequest) => {
   try {
     const token = await req.text();
-    const res = await fetch("http://localhost:8000/api/auth/CreateUser", {
-      method: "POST",
-      credentials: "include",
-      body: JSON.stringify(token),
-    });
-    const body = await res.json();
-    console.log(body);
+    // const res = await fetch("http://localhost:8000/api/auth/CreateUser", {
+    //   method: "POST",
+    //   credentials: "include",
+    //   body: JSON.stringify(token),
+    // });
+    // const body = await res.json();
+    // console.log(body);
+    console.log(token);
     return NextResponse.json("hello");
   } catch (error) {
     console.log(error);

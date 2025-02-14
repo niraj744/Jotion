0ede3e272e17b16713d4b04905d93955b5f4dd8c
niraj <nirajversilal@gmail.com>
2025-02-14 17:34:29 +0500

    webhook deploy

--- app/api/auth/CreateUser/route.ts
+++ app/api/auth/CreateUser/route.ts
@@ -1,21 +1,19 @@
-// import axios from "axios";
+import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest) => {
   try {
     const token = await req.text();
-    console.log(token);
-    // const res = await axios.post(
-    //   "https://jotion-backend.osc-fr1.scalingo.io/api/auth/CreateUser",
-    //   {
-    //     token,
-    //   },
-    //   {
-    //     withCredentials: true,
-    //   }
-    // );
-    // console.log(res);
-    return NextResponse.json(token);
+    await axios.post(
+      "https://jotion-backend.osc-fr1.scalingo.io/api/auth/CreateUser",
+      {
+        token,
+      },
+      {
+        withCredentials: true,
+      }
+    );
+    return NextResponse.json("actions performed successfully");
   } catch (error) {
     console.log(error);
   }
